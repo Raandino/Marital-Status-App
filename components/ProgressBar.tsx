282cb7172481016e4afe12ad13b71715896fa720
Rodolfo Andino <aandino186@gmail.com>
2024-10-04 09:26:29 -0600

    Fix(ProgressBar): Added fixed size

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -6,8 +6,8 @@ type Props = {
 
 export default function ProgressBar({ progress }: Props) {
   return (
-    <View className="w-[179px] h-[6px] bg-neutral-2 rounded-full overflow-hidden">
-      <View className={`h-full w-[${progress}%] bg-appBlue rounded-full`} />
+    <View className="w-[179px] h-[6px] bg-neutral-2 relative rounded-full overflow-hidden">
+      <View className={`h-full w-[15%] bg-appBlue rounded-full`} />
     </View>
   );
 }
